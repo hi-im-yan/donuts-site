9e30761e5b0e5a7f8964f8458b5c95f04e16c781
yan <yanajiki2010@hotmail.com>
2024-12-30 21:30:32 -0400

    feat: add price description to stuffed and non stuffed donuts

--- src/main/java/com/yanajiki/yuyudonuts/domain/DonutController.java
+++ src/main/java/com/yanajiki/yuyudonuts/domain/DonutController.java
@@ -19,10 +19,10 @@ public class DonutController {
         );
 
         List<DonutPackage> packages = List.of(
-                new DonutPackage("4 unidades", "R$ 6,00", "/images/4-donuts.jpg", 4),
-                new DonutPackage("8 unidades", "R$ 8,00", "/images/8-donuts.jpg", 8),
-                new DonutPackage("16 unidades", "R$ 12,00", "/images/16-donuts.jpg", 16),
-                new DonutPackage("20 unidades", "R$ 22,00", "/images/20-donuts.jpg", 20)
+                new DonutPackage("4 unidades", "R$ 6,00 Sem Recheio \n R$ 9,00 Com Recheio", "/images/4-donuts.jpg", 4),
+                new DonutPackage("8 unidades", "R$ 8,00 Sem Recheio \n R$13,00 Com Recheio", "/images/8-donuts.jpg", 8),
+                new DonutPackage("16 unidades", "R$12,00 Sem Recheio \n R$15,00 Com Recheio", "/images/16-donuts.jpg", 16),
+                new DonutPackage("20 unidades", "R$22,00 Sem Recheio \n R$25,00 Com Recheio", "/images/20-donuts.jpg", 20)
         );
         model.addAttribute("donuts", donuts);
         model.addAttribute("logoImage", "/images/logo.jpg");
